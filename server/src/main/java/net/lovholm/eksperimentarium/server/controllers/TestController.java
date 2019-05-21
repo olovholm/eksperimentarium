ce4cc01ad4a3c81c43969145015470f154de0b70
Ola Lovholm <ola@lovholm.net>
2019-05-22 00:31:35 +0200

    Fix CircleCI + paths i REST-controller

--- server/src/main/java/net/lovholm/eksperimentarium/server/controllers/TestController.java
+++ server/src/main/java/net/lovholm/eksperimentarium/server/controllers/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("test")
+@RequestMapping("/test")
 public class TestController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/")

0fe6e9992ec3c5af1df70cbffdc48a323a8fe5a2
olovholm <ola.lovholm@nav.no>
2019-07-12 19:01:47 +0200

    Endrer versjonering og import. Enkel data fra endepunkt for å teste back-end -> front-end mapping

--- server/src/main/java/net/lovholm/eksperimentarium/server/controllers/TestController.java
+++ server/src/main/java/net/lovholm/eksperimentarium/server/controllers/TestController.java
@@ -1,11 +1,15 @@
 package net.lovholm.eksperimentarium.server.controllers;
 
 
+import net.lovholm.eksperimentarium.domene.Person;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/test")
@@ -16,6 +20,19 @@ public class TestController {
 
         return ResponseEntity.ok().body("Hello");
 
+
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/alle")
+    public List<Person> getPersoner() {
+
+        List<Person> personer = new ArrayList<>();
+        personer.add(new Person("test", "testersen","03923823"));
+        personer.add(new Person("sadg", "testersen","235"));
+        personer.add(new Person("tedfskldsst", "testersen","039252352353823"));
+        personer.add(new Person("tagdest", "testersen","235235"));
+
+        return personer;
 
     }
 

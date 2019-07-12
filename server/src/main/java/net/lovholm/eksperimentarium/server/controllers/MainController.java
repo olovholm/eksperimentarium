78cd6a898da4f6c02a42716a404606a8537648cd
olovholm <ola.lovholm@nav.no>
2019-07-12 18:11:28 +0200

    Legger til front-end

--- server/src/main/java/net/lovholm/eksperimentarium/server/controllers/MainController.java
+++ server/src/main/java/net/lovholm/eksperimentarium/server/controllers/MainController.java
@@ -2,20 +2,17 @@ package net.lovholm.eksperimentarium.server.controllers;
 
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class MainController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/")
-    public ResponseEntity index() {
-        return ResponseEntity.ok().body("Hello from basic app");
-
-        }
 
 
 }
